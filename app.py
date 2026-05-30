21679898aba91d0251ee922485ff766c5854a53c
Ahmed <ezzitia@gmail.com>
2026-05-30 23:17:38 +0300

    Fix: change routing port from 5000 to official cloud port 7860

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello DevOps World! This is version 1.0"
+    return "Hello DevOps World! This is version 2.0"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    # قمنا بتغيير المنفذ هنا إلى 7860 ليتوافق مع الأنظمة السحابية
+    app.run(host='0.0.0.0', port=7860)
